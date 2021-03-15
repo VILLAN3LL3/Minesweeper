f0f98caebde0791d9b15f8f55d9f46914875f299
Mira Manger <mira.manger@haufe-lexware.com>
2021-03-15 16:04:16 +0100

    Projektdateien hinzufügen.

---
+++ Minesweeper/MinesweeperCell.cs
@@ -0,0 +1,10 @@
+﻿namespace Minesweeper
+{
+    public class MinesweeperCell
+    {
+        public int ColumnCoord { get; set; }
+        public int RowCoord { get; set; }
+        public bool HasMine { get; set; }
+        public int NeighbourMineCount { get; set; }
+    }
+}
